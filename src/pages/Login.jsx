13e55eb86b4a96e5fda4de31129b6b641326ec5b
MaxDM <maxdiazm.2004@gmail.com>
2026-04-27 16:03:12 -0600

    login page changed (removed navbar & footer)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,9 @@
-import Layout from "../components/layout";
 function Login(params) {
   return (
     <>
-      <Layout>
-        <div className="h-screen text-center p-8">
-          <h1>Pagina de Login</h1>
-        </div>
-      </Layout>
+      <div className="h-screen text-center p-8">
+        <h1>Pagina de Login</h1>
+      </div>
     </>
   );
 }
